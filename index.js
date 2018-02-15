b958c575bbb71c3945dcb319b325e04f78ff8970
Kalle Perkoila <kalle.perkoila@helsinki.fi>
2018-02-15 18:50:32 +0200

    4.18

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose')
 const usersRouter = require('./controllers/users')
 const blogsRouter = require('./controllers/blogs')
 const config = require('./utils/config')
+const loginRouter = require('./controllers/login')
 
 mongoose
     .connect(config.mongoUrl)
@@ -23,6 +24,7 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 const server = http.createServer(app)
 
