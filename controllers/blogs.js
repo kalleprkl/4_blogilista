b1738a2844cb6376d23e48525db95896c425417d
Kalle Perkoila <kalle.perkoila@helsinki.fi>
2018-02-15 19:08:39 +0200

    4.19

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,5 +1,14 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = (request) => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        return authorization.substring(7)
+    }
+    return null
+}
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog
@@ -12,21 +21,37 @@ blogsRouter.post('/', async (request, response) => {
 
     const body = request.body
 
-    if (body.title === undefined || body.url === undefined) {
-        return response.status(400).json('Bad request')
+    try {
+        const token = getTokenFrom(request)
+        const decodedToken = jwt.verify(token, process.env.SECRET)
+
+        if (!token || !decodedToken.id) {
+            return response.status(401).json({ error: 'token missing or invalid' })
+        }
+
+        if (body.title === undefined || body.url === undefined) {
+            return response.status(400).json('Bad request')
+        }
+
+        const blog = new Blog({
+            title: body.title,
+            author: body.author,
+            url: body.url,
+            likes: body.likes === undefined ? 0 : body.likes,
+            user: decodedToken.id
+        })
+
+        const savedBlog = await blog.save()
+
+        response.status(201).json(savedBlog)
+    } catch (exception) {
+        if (exception.name === 'JsonWebTokenError') {
+            response.status(401).json({ error: exception.message })
+        } else {
+            console.log(exception)
+            response.status(500).json({ error: 'something went wrong...' })
+        }
     }
-
-    const blog = new Blog({
-        title: body.title,
-        author: body.author,
-        url: body.url,
-        likes: body.likes === undefined ? 0 : body.likes,
-        user: body.user
-    })
-
-    const savedBlog = await blog.save()
-
-    response.status(201).json(savedBlog)
 
 })
 
